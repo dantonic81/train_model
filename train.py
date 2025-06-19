571d701c2476af9e31f13010470e3428536fc6c0
Dean <imadeureadis@gmail.com>
2025-06-19 15:17:17 +0200

    add model and training pipeline

---
+++ train.py
@@ -0,0 +1,30 @@
+# train.py
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from model import MNISTModel
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+dataset = datasets.MNIST('.', train=True, download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+model = MNISTModel()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+epochs = 2
+for epoch in range(epochs):
+    for images, labels in dataloader:
+        optimizer.zero_grad()
+        output = model(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item():.4f}")
+
+torch.save(model.state_dict(), 'model.pth')
